4080a66e78c9c04d11fe5af837cb9d7addd10ece
jesus9702 <jdmendieta@sanmateo.edu.co>
2018-03-07 11:24:56 -0500

    Saludo
    archivo .bat

---
+++ aplicaciones Moviles.bat
@@ -0,0 +1,8 @@
+@echo off
+title Aplicaciones Moviles
+echo Buena profesor German Sarmiento 
+echo Nombre : Jesus David Mendieta Plaza
+echo Grupo : 01N30
+echo Carrera : Ingenieria de Sistemas
+pause>nul
+exit
